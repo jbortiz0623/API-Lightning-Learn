b8cef436a816612fd2e2ebc37c86c45f1303d560
jbortiz0623 <jbortiz0623@gmail.com>
2018-11-19 10:35:55 -0800

    inserted and modified all files

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,32 @@
 // Jquery is already set up for you
+
+
+var jq = jQuery.noConflict();
+
+jq.ajax({
+  url: 'https://api.openaq.org/v1/countries',
+  method: 'GET',
+  success: function(response) {
+      console.log(response)
+      console.log(response.results)
+
+    jq('#space').empty();
+    var cities = response.results
+    
+    console.log(cities)
+            cities.forEach(ozone => {
+
+                let elem = `<p>${ozone.name} population is ${ozone.count} and has ${ozone.locations} different locations with poor air quality.</p><br>`
+                console.log(ozone.locations);
+
+                jq('#space').append(elem)
+            });
+  },
+
+
+    
+    error: function(error) { 
+ 
+}
+});
+
